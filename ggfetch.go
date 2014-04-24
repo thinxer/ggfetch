a91224203f73dd0585e62e30bfb87e996abf51a0
Jianfei Wang <wangjf@hohoyi.com>
2014-04-24 15:08:36 +0800

    treat non-200 status code as error

--- ggfetch.go
+++ ggfetch.go
@@ -2,6 +2,7 @@ package ggfetch
 
 import (
 	"bytes"
+	"fmt"
 	"hash/crc32"
 	"io"
 	"log"
@@ -13,6 +14,15 @@ import (
 	"github.com/golang/groupcache"
 )
 
+type StatusCodeError struct {
+	URL  string
+	Code int
+}
+
+func (r StatusCodeError) Error() string {
+	return fmt.Sprintf("Response code %d for URL: %s", r.Code, r.URL)
+}
+
 func fetch(client *http.Client, url string, maxSize int64) ([]byte, error) {
 	log.Println("Fetching", url)
 	resp, err := client.Get(url)
@@ -20,9 +30,11 @@ func fetch(client *http.Client, url string, maxSize int64) ([]byte, error) {
 		return nil, err
 	}
 	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, StatusCodeError{url, resp.StatusCode}
+	}
 
 	buf := new(bytes.Buffer)
-
 	// Check Content Type
 	io.CopyN(buf, resp.Body, 512)
 	contentType := http.DetectContentType(buf.Bytes())
